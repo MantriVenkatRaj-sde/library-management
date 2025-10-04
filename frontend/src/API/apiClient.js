4691e24a1ff3948adba9c79e0ada3af70232ad85
Mantri Venkat Raj <rajmv69@gmail.com>
2025-10-04 13:04:59 +0530

    introduced an environment variable for base URL

--- frontend/src/API/apiClient.js
+++ frontend/src/API/apiClient.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
+const BASE_URL = process.env.REACT_APP_API_BASEURL;
 export const apiClient = axios.create({
-    baseURL: "http://bookcircle-sprinboot-server-alb-1527815027.ap-south-1.elb.amazonaws.com:8080"
+  baseURL: BASE_URL,
 });
