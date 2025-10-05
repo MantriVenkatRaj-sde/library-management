3563e13f2c7a6a4b60172b494e054e6b164b2220
Mantri Venkat Raj <rajmv69@gmail.com>
2025-10-05 11:25:08 +0530

    minor tweaks

--- frontend/src/API/apiClient.js
+++ frontend/src/API/apiClient.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 export const BASE_URL = process.env.NODE_ENV === "production"
-    ? REACT_APP_API_URL // deployed backend URL
-    : "http://localhost:8002"; // local Spring Boot backend
+    ? process.env.REACT_APP_API_URL
+    : "http://localhost:8002";
+    
 export const apiClient = axios.create({
 //   baseURL: BASE_URL,
     baseURL: BASE_URL
