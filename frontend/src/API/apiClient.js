67853b43c881399b43aa08c2e68205bb224bbb95
Mantri Venkat Raj <rajmv69@gmail.com>
2025-10-05 11:30:39 +0530

    minor tweaks

--- frontend/src/API/apiClient.js
+++ frontend/src/API/apiClient.js
@@ -1,10 +1,8 @@
 import axios from "axios";
 
-export const BASE_URL = process.env.NODE_ENV === "production"
-    ? process.env.REACT_APP_API_URL
-    : "http://localhost:8002";
-    
+export const BASE_URL =process.env.REACT_APP_API_URL
+
 export const apiClient = axios.create({
-//   baseURL: BASE_URL,
-    baseURL: BASE_URL
+  baseURL: BASE_URL,
+  
 });
